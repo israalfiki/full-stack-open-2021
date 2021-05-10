fbbe59bb190ffec3ff900c429fa453bd7eb10ab8
israalfiki <35269342+israalfiki@users.noreply.github.com>
2021-05-10 03:28:39 +0200

    added exercise 1.4

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -40,34 +40,38 @@ const Total = (props) => {
 
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = {
-    name:"Fundamentals of React",
-    exercises:10
-  }
-  const part2 = {
-    name:"Using props to pass data",
-    exercises:7
-  }
-  const part3= {
-    name:"State of a component",
-    exercises:14
-  }
-  
+  const part = [
+    {
+      name:"Fundamentals of React",
+      exercises:10
+    },
+    {
+      name:"Using props to pass data",
+      exercises:7
+    },
+    {
+      name:"State of a component",
+      exercises:14
+    }
+
+
+  ]
+
   return (
     <div>
       <Header course={course} />
       <Content
-        part1={part1.name}
-        part2={part2.name}
-        part3={part3}
-        exercises1={part1.exercises}
-        exercises2={part2.exercises}
-        exercises3={part3.exercises}
+        part1={part[0].name}
+        part2={part[1].name}
+        part3={part[2].name}
+        exercises1={part[0].exercises}
+        exercises2={part[1].exercises}
+        exercises3={part[2].exercises}
       />
       <Total
-        exercises1={part1.exercises}
-        exercises2={part2.exercises}
-        exercises3={part3.exercises}
+        exercises1={part[0].exercises}
+        exercises2={part[1].exercises}
+        exercises3={part[2].exercises}
       />
     </div>
   );
